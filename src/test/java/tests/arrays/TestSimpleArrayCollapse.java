655c94d0d012861e4bccab559465111bb8c4562b
afarrukh125 <33581953+afarrukh125@users.noreply.github.com>
2021-04-28 03:27:07 +0100

    Add test case and update for next potential example.

---
+++ src/test/java/tests/arrays/TestSimpleArrayCollapse.java
@@ -0,0 +1,9 @@
+package tests.arrays;
+
+import tests.GenericTest;
+
+public class TestSimpleArrayCollapse extends GenericTest {
+    public TestSimpleArrayCollapse() {
+        super("ArrayCreation.ald", 6);
+    }
+}
