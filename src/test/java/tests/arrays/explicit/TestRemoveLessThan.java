38be4520a9a9f8f1f74150f78d110d821b83f626
afarrukh125 <33581953+afarrukh125@users.noreply.github.com>
2021-05-03 18:50:19 +0100

    Update file name in test case as well. Add ANTLR jars as needed to bin folder.

--- src/test/java/tests/arrays/explicit/TestRemoveLessThan.java
+++ src/test/java/tests/arrays/explicit/TestRemoveLessThan.java
@@ -4,7 +4,7 @@ import tests.ArrayTest;
 
 public class TestRemoveLessThan extends ArrayTest {
     public TestRemoveLessThan() {
-        super("ArrayLessThan.ald", new ArrayStructureBuilder<Integer>()
+        super("ArrayRemoveLessThan.ald", new ArrayStructureBuilder<Integer>()
                 .addArray(2, 15, 5, 6, 7, 8, 28, 24, 21)
                 .filter(x -> x>=6)
                 .build());
